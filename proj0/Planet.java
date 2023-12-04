f29e633b083fbd8eb3b1f1a193c1fc99f23b94cf
sOda16 <2446745341@qq.com>
2023-12-04 21:38:16 +0800

    upload calcNetForceExertedByXY

---
+++ proj0/Planet.java
@@ -0,0 +1,78 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    private static final double G = 6.67e-11;
+
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    
+    public Planet(Planet p) {
+        xxPos = p.xxPos;
+        yyPos = p.yyPos;
+        xxVel = p.xxVel;
+        yyVel = p.yyVel;
+        mass = p.mass;
+        imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.sqrt((xxPos - p.xxPos) * (xxPos - p.xxPos) + (yyPos - p.yyPos) * (yyPos - p.yyPos));
+    }
+
+    public double calcForceExertedBy(Planet p) {
+        double r = calcDistance((p));
+        return G * mass * p.mass / (r * r);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        double F = calcForceExertedBy(p);
+        double dx = p.xxPos - xxPos;
+        double r = calcDistance(p);
+        return  F * dx / r;
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        double F = calcForceExertedBy(p);
+        double dy = p.yyPos - yyPos;
+        double r = calcDistance(p);
+        return F * dy / r;
+    }
+
+    public double calcNetForceExertedByX(Planet[] allPlanets) {
+        double totalForceX = 0;
+        for (Planet planet : allPlanets) {
+            if (this.equals(planet)) {
+                continue;
+            } else {
+                totalForceX += calcForceExertedByX(planet);
+            }
+        }
+
+        return  totalForceX;
+    }
+
+    public double calcNetForceExertedByY(Planet[] allPlanets) {
+        double totalForceY = 0;
+        for (Planet planet : allPlanets) {
+            if (this.equals((planet))) {
+                continue;
+            } else {
+                totalForceY += calcForceExertedByY(planet);
+            }
+        }
+
+        return totalForceY;
+    }
+
+}
